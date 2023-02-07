7e8519ce15943cde82630599f5d5c86d90ddac1e
VikaPetrusevich <viktoriaklapkova@gmail.com>
2023-02-07 15:58:31 +0100

    feat: add date and time

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const time = document.querySelector('.time');
+
+function showTime() {
+    const date = new Date();
+    const currentTime = date.toLocaleTimeString();
+    time.textContent = currentTime;
+    setTimeout(showTime, 1000);
+}
+showTime();
+
+const dated = document.querySelector('.date');
+
+function showDate() {
+    const date = new Date();
+    const options = { weekday: 'long', month: 'long', day: 'numeric', timeZone: 'UTC' };
+    const currentDate = date.toLocaleDateString('en-US', options);
+    dated.textContent = currentDate;
+    setTimeout(showDate, 1000);
+}
+showDate();
+
+
