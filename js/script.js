25a7272a1878f002e565655859c43c5a6501da72
VikaPetrusevich <viktoriaklapkova@gmail.com>
2023-02-08 15:28:16 +0100

    feat: add greeting

--- js/script.js
+++ js/script.js
@@ -19,4 +19,55 @@ function showDate() {
 }
 showDate();
 
+const text = document.querySelector('.greeting');
+
+function showGreeting(){
+    getTimeOfDay();
+    setTimeout(showGreeting, 1000);
+}
+showGreeting();
+
+function getTimeOfDay() {
+    const date = new Date();
+    const hours = date.getHours();
+
+    if (hours >= 5 && hours < 12) {
+        const timeOfDay = 'Morning';
+        const greetingText = `Good ${timeOfDay},`;
+        text.textContent = greetingText;
+    }
+    if (hours >= 12 && hours < 18) {
+        const timeOfDay = 'Afternoon';
+        const greetingText = `Good ${timeOfDay},`;
+        text.textContent = greetingText;
+    }
+    if (hours >= 18 && hours < 21) {
+        const timeOfDay = 'Evening';
+        const greetingText = `Good ${timeOfDay},`;
+        text.textContent = greetingText;
+    }
+    if (hours >= 21 && hours < 5) {
+        const timeOfDay = 'Night';
+        const greetingText = `Good ${timeOfDay},`;
+        text.textContent = greetingText();
+    }
+}
+
+function setLocalStorage() {
+    let name = document.querySelector('.name');
+    localStorage.setItem('name', name.value);
+  }
+
+  window.addEventListener('beforeunload', setLocalStorage);
+
+  function getLocalStorage() {
+    let name = document.querySelector('.name');
+    if(localStorage.getItem('name')) {
+      name.value = localStorage.getItem('name');
+    }
+  }
+  window.addEventListener('load', getLocalStorage)
+
+
+
 
