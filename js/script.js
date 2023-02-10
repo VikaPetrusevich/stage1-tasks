ac497265652bf633395822317c6dc6dd5d603b35
VikaPetrusevich <viktoriaklapkova@gmail.com>
2023-02-10 15:15:16 +0100

    feat:add weather widget

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ function getTimeOfDay() {
     let greetingText;
     let timeOfDay;
 
-    if (hours >= 5 && hours < 12) {
+    if (hours >= 6 && hours < 12) {
         timeOfDay = 'Morning';
         greetingText = `Good ${timeOfDay},`;
         text.textContent = greetingText;
@@ -43,12 +43,12 @@ function getTimeOfDay() {
         greetingText = `Good ${timeOfDay},`;
         text.textContent = greetingText;
     }
-    if (hours >= 18 && hours < 21) {
+    if (hours >= 18 && hours < 24) {
         timeOfDay = 'Evening';
         greetingText = `Good ${timeOfDay},`;
         text.textContent = greetingText;
     }
-    if (hours >= 21 && hours < 5) {
+    if (hours >= 0 && hours < 6) {
         timeOfDay = 'Night';
         greetingText = `Good ${timeOfDay},`;
         text.textContent = greetingText();
@@ -76,20 +76,21 @@ let randomNum = (Math.floor(Math.random() * 20) + 1);
 
 function setBg() {
     let timeOfDay = getTimeOfDay().toLowerCase();
-    let bgNum = randomNum.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+    let bgNum = randomNum.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
     const body = document.body;
     const img = new Image();
     img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`;
-    img.onload = () => { 
-    body.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg')`};
+    img.onload = () => {
+        body.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg')`
+    };
 }
 setBg();
 
 const slideNext = document.querySelector('.slide-next.slider-icon');
 const slidePrev = document.querySelector('.slide-prev.slider-icon');
 
-function getSlideNext(){
-    if (randomNum === 20){
+function getSlideNext() {
+    if (randomNum === 20) {
         randomNum = 1;
     }
     else {
@@ -100,18 +101,50 @@ function getSlideNext(){
 
 slideNext.addEventListener('click', getSlideNext);
 
-function getSlidePrev(){
-    if (randomNum === 1){
+function getSlidePrev() {
+    if (randomNum === 1) {
         randomNum = 20;
     }
     else {
-        randomNum = randomNum-1;
+        randomNum = randomNum - 1;
     }
     setBg();
 }
 
 slidePrev.addEventListener('click', getSlidePrev);
 
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const weatherDescription = document.querySelector('.weather-description');
+
+let city = document.querySelector('.city');
+
+async function getWeather() {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=08f2a575dda978b9c539199e54df03b0&units=metric`;
+    const res = await fetch(url);
+    const data = await res.json();
+
+    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    temperature.textContent = `${data.main.temp}°C`;
+    weatherDescription.textContent = data.weather[0].description;
+}
+getLocalStorageCity();
+getWeather();
+
+function setLocalStorageCity() {
+    localStorage.setItem('city', city.value);
+}
+
+function getLocalStorageCity() {
+    if (localStorage.getItem('city')) {
+        city.value = localStorage.getItem('city');
+    }
+}
+
+window.addEventListener('beforeunload', setLocalStorageCity);
+window.addEventListener('load', getLocalStorageCity)
+
+city.onchange = getWeather;
 
 
 
