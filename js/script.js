ab0506ca68ed4140cfa9f77d02458300e2bce148
VikaPetrusevich <viktoriaklapkova@gmail.com>
2023-02-24 10:29:17 +0100

    feat: add audio player

--- js/script.js
+++ js/script.js
@@ -212,7 +212,7 @@ function getPlayPrev() {
 
 playPrev.addEventListener('click', getPlayPrev);
 
-import playList from './playlist.js';
+import playList from './playList.js';
 console.log(playList);
 
 const ul = document.querySelector('.play-list');
